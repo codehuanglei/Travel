4424a67c3a38c67755dda0e58233c912be6813ac
codehuanglei <1142372633@qq.com>
2020-05-10 19:18:42 +0800

    homehead update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+import './assets/styles/iconfont.css'
+import fastClick from 'fastclick';
 Vue.config.productionTip = false
-
+fastClick.attach(document.body)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class='header'>
+        <div class='header-search'>
+            <div class='header-input'>
+            <i class='iconfont search-icon'>&#xe629;</i>
+            <div class='header-searchhint'>
+                <span>搜索景点门票</span>
+            </div>
+            </div>
+        </div>
+        <div class='header-right'>
+        <i class='iconfont download-icon'>&#xe673;</i>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:'Header'
+}
+</script>
+<style lang="stylus" scoped>
+    .header
+        min-width :320px !important
+        height :.36rem
+        margin:.15rem .16rem 0
+        padding-bottom :.1rem
+        display :flex
+        .header-search
+            height: .36rem;
+            width: 2.91rem;
+            border-radius: 3px;
+            border: 0 none;
+            box-sizing: border-box;
+            background-color: #eee;
+            .header-input
+                width: calc(100% - 50px);
+                background-color: #eee;
+                margin: 0 10px 0 0;
+                height: 100%;
+                border: 0 none;
+                line-height: 30px;
+                font-size: 20px;
+                display: flex;
+                .search-icon
+                    line-height :.36rem
+                    color:#a5a5a5
+                    margin-left :.06rem
+                    font-size :.20rem
+                .header-searchhint
+                    font-size: .15rem;
+                    margin-left: .06rem;
+                    color: #a5a5a5;
+                    line-height: .36rem;
+                    display: inline-block;
+                    width: 100%;
+                    overflow: hidden;
+                    text-overflow: ellipsis;
+                    white-space: nowrap;
+        .header-right
+            display: initial;
+            width: 47px;
+            height: 38px;
+            text-align: center;
+            margin-left: 16px;
+            .download-icon
+                line-height :.36rem
+                font-size:.24rem
+                color:#a5a5a5
+</style>
